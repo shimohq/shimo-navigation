f1bda4fb41b8555853fffd109d5cdd176b8d0331
陈阳 <chenyang@shimo.im>
2019-04-03 18:56:18 +0800

    chore: add getDefaultParams for TabRouter

--- src/routers/TabRouter.js
+++ src/routers/TabRouter.js
@@ -8,6 +8,33 @@ export default (routeConfigs, stackConfig) => {
     routeConfigs,
     stackConfig.navigationOptions
   );
+  
+  getStateForAction = router.getStateForAction;
+
+  router.getStateForAction = function (action, inputState) {
+    const state = getStateForAction(action, inputState);
+    if (!inputState) {
+        const routes = state.routes.map((route) => {
+          const { routeName } = route;
+          const getDefaultParams = routeConfigs[routeName].getDefaultParams;
+
+          return {
+            ...route,
+            params: getDefaultParams ? {
+              ...getDefaultParams(),
+              ...route.params
+            } : route.params
+          }
+        });
+      return {
+        ...state,
+        routes
+      }
+    } else {
+      return state;
+    }
+ 
+  }
 
   return router;
 }
